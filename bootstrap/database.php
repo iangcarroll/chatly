04d8032049903277e8de04d01565bcf58ff7d129
Ian Carroll <iangcarroll+spam-only@icloud.com>
2015-11-02 22:20:39 -0500

    Update database.php

--- bootstrap/database.php
+++ bootstrap/database.php
@@ -7,20 +7,32 @@ use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
 $capsule = new Capsule;
 $fs = new Filesystem();
-$path = getenv("HOME") . '/.chatly-data/chatly.sqlite';
-if (! $fs->exists(getenv("HOME") . '/.chatly-data')) {
-  $fs->mkdir(getenv("HOME") . '/.chatly-data');
+if (getenv("DATABASE_URL")) {
+    $url = parse_url(getenv("DATABASE_URL"));
+    $capsule->addConnection([
+        'driver'    => 'pgsql',
+        'database'  => $url["path"],
+        'username'  => $url["user"],
+        'password'  => $url["pass"],
+        'host'      => $url["host"],
+        'port'      => $url["port"]
+    ]);
+} else {
+    $path = getenv("HOME") . '/.chatly-data/chatly.sqlite';
+    if (! $fs->exists(getenv("HOME") . '/.chatly-data')) {
+        $fs->mkdir(getenv("HOME") . '/.chatly-data');
+    }
+    if (! $fs->exists($path)) {
+        try {
+            $fs->touch($path);
+        } catch (IOExceptionInterface $e) {
+            throw new RuntimeException("Error creating a database at " . $path);
+        }
+    }
+    $capsule->addConnection([
+      'driver'    => 'sqlite',
+      'database'  => $path
+    ]);
 }
-if (! $fs->exists($path)) {
-  try {
-      $fs->touch($path);
-  } catch (IOExceptionInterface $e) {
-      throw new RuntimeException("Error creating a database at " . $path);
-  }
-}
-$capsule->addConnection([
-    'driver'    => 'sqlite',
-    'database'  => $path
-]);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
