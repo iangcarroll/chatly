8e01244e315b4d4cc9847ec31821819ef316b7ab
Ian Carroll <iangcarroll+spam-only@icloud.com>
2016-05-09 21:09:59 -0400

    Update formatting.

--- chat.php
+++ chat.php
@@ -6,5 +6,7 @@ require __DIR__.'/bootstrap/app.php';
 use App\Command\Kernel as CommandKernel;
 
 $application = new Chatly('Chatly', '1.0');
+
 $application->load(new CommandKernel());
+
 $application->run();
