496d83d3573f149b2b5bf205c460d8ffbc8da1ab
Ian Carroll <iangcarroll+spam-only@icloud.com>
2016-05-09 21:25:30 -0400

    Fix broken indentation.

--- src/App/Responder/Kernel.php
+++ src/App/Responder/Kernel.php
@@ -5,8 +5,8 @@ namespace App\Responder;
 class Kernel extends ResponderKernel
 {
     public $responders = [
-    HelpResponder::class,
-    FactResponder::class,
-    GiphyResponder::class,
-  ];
+        HelpResponder::class,
+        FactResponder::class,
+        GiphyResponder::class,
+    ];
 }
